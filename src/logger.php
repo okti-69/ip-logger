c511627f2450853ff7559be8fd271467bf0479dd
okti <62831711+okti-69@users.noreply.github.com>
2021-01-25 12:06:14 +0100

    Update logger.php

--- src/logger.php
+++ src/logger.php
@@ -1,1 +1,29 @@
+<?php
+if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+    $ip = $_SERVER['HTTP_CLIENT_IP'];
+} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+} else {
+    $ip = $_SERVER['REMOTE_ADDR'];
+}
+$ipdat = @json_decode(file_get_contents( 
+    "http://ip-api.com/json/".$ip."?fields=status,message,country,city,regionName")); 
+    if ($ipdat->status === "fail"){
+        $kraj = "Error";
+        $miasto = $ipdat->message;
+        $region = "Error";
+    }
+    elseif ($ipdat->status === "success"){
+        $kraj = $ipdat->country;
+        $miasto = $ipdat->city;
+        $region = $ipdat->regionName;
+    }
+if ("$kraj" == "Poland"){
+  $class = "poland";
+} else {
+  $class = "server"; 
+}
+$aktualne = file_get_contents('/ips.html');
+file_put_contents('/ips.html', "$aktualne <br><br><div class='$class'>(".date("d-m-Y h:i:s").") $ip || $kraj, $miasto || ".$_SERVER['HTTP_USER_AGENT']."</div>");
+ ?>
 
