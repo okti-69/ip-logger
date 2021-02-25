05321033c103fd590e32b025ab0c47eac18eddfa
okti <62831711+okti-69@users.noreply.github.com>
2021-02-25 12:32:48 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <?php
-require "/src/logger.php";	
-require "/src/przekierowanie.php";
-if ($_GET["redirect"]){
-	redirect($_GET["redirect"]);
+require "src/logger.php";	
+if (@$_GET["redirect"]){
+	header("Location: ".$_GET["redirect"]);
 } else {
 	echo "Error 404: Not found";
 }?>
